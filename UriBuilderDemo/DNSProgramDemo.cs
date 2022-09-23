5b4cffcce4c067135f1e26c5394c50049c1ecae4
Rio <rio.imam@gmail.com>
2022-09-23 12:55:36 +0700

    Initial Commit 2

---
+++ UriBuilderDemo/DNSProgramDemo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+namespace UriBuilderDemo
+{
+    class DNSProgramDemo
+    {
+        static void Main(string[] args)
+        {            
+            
+            var domainEntry = Dns.GetHostEntry("facebook.com");
+  
+            Console.WriteLine(domainEntry.HostName);
+            foreach (var ip in domainEntry.AddressList)
+            {
+                Console.WriteLine(ip);
+            }
+
+            domainEntry = Dns.GetHostEntry(Dns.GetHostName());
+            Console.WriteLine(domainEntry.HostName);
+            foreach (var ip in domainEntry.AddressList)
+            {
+                Console.WriteLine(ip);
+            }
+
+            IPAddress ipadd = IPAddress.Parse("142.251.10.95");
+            var ipEntry = Dns.GetHostEntry(ipadd);
+            foreach (var ip in ipEntry.AddressList)
+            {
+                Console.WriteLine(ip);
+            }
+
+            Thread.Sleep(10000);
+
+        }
+    }
+}
